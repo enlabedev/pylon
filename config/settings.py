eafe0f325342965bda9459b9c437d9fe4818329b
Enrique Lazo Bello <enlabe@gmail.com>
2025-05-04 00:43:30 -0500

    Add BuildingsConfig to INSTALLED_APPS in settings

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "apps.users.apps.UsersConfig",
     "apps.core.apps.CoreConfig",
+    "apps.buildings.apps.BuildingsConfig",
     "guardian",
 ]
 
